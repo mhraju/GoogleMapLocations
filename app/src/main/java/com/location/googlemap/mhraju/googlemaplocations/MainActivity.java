c73d3e77f6c9c5245e14d8ef42fe5d941128823d
YOUR NAME <YOUR EMAIL ADDRESS>
2016-08-20 23:26:34 +0600

    Google Map Location with Polyline
    Google Map Location with Polyline

---
+++ app/src/main/java/com/location/googlemap/mhraju/googlemaplocations/MainActivity.java
@@ -0,0 +1,172 @@
+package com.location.googlemap.mhraju.googlemaplocations;
+
+import java.util.ArrayList;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    ArrayList<LatLng> points;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpMapIfNeeded();
+
+
+
+        points = new ArrayList<LatLng>();
+
+        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mMap = fm.getMap();
+
+        mMap.setMyLocationEnabled(true);
+
+        mMap.setOnMapClickListener(new OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng point) {
+
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                markerOptions.position(point);
+
+                markerOptions.title("Position");
+
+                markerOptions.snippet("Latitude:"+point.latitude+","+"Longitude:"+point.longitude);
+
+                PolylineOptions polylineOptions = new PolylineOptions();
+
+                polylineOptions.color(Color.RED);
+
+                polylineOptions.width(3);
+
+                points.add(point);
+
+                polylineOptions.addAll(points);
+
+                mMap.addPolyline(polylineOptions);
+
+                mMap.addMarker(markerOptions);
+
+            }
+        });
+
+        mMap.setOnMapLongClickListener(new OnMapLongClickListener() {
+
+            @Override
+            public void onMapLongClick(LatLng point) {
+
+                mMap.clear();
+
+                points.clear();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+
+    public void onSearch(View view)
+    {
+        EditText location_tf = (EditText)findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+        List<Address> addressList = null;
+        if(location != null || !location.equals(""))
+        {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location , 1);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude() , address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        }
+    }
+
+    public void onZoom(View view)
+    {
+        if(view.getId() == R.id.Bzoomin)
+        {
+            mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        }
+        if(view.getId() == R.id.Bzoomout)
+        {
+            mMap.animateCamera(CameraUpdateFactory.zoomOut());
+        }
+    }
+
+    public void changeType(View view)
+    {
+        if(mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL)
+        {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        else
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.7516, 90.3943)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
+
+
+    }
+}
